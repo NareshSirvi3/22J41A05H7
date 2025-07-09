570e6a8ef8d6c2eca0e02ca7e5672dbf7e8893da
Naresh Sirvee <nareshsirvi726@gmail.com>
2025-07-09 12:41:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.get('/shorturls/:shortCode', auth, async (req, res) => {
 });
 
 mongoose.connect(MONGO_URI).then(() => {
-  console.log('✅ MongoDB connected');
+  console.log('MongoDB connected');
   app.listen(PORT, () => console.log(` Server running at http://localhost:${PORT}`));
 }).catch(err => console.error(' MongoDB connection failed:', err));
